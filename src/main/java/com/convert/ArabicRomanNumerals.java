a72804085ac379f6e5bddef941738f6b114bd7d2
dede376-TECH <khamlicheabdelghani5@gmail.com>
2024-12-18 22:25:08 +0100

    Mise à jour de la classe ArabicRomanNumerals avec la méthode de conversion des entiers en chiffres romains

--- src/main/java/com/convert/ArabicRomanNumerals.java
+++ src/main/java/com/convert/ArabicRomanNumerals.java
@@ -1,24 +1,20 @@
 package com.convert;
 
 public class ArabicRomanNumerals {
+    public static String convert(int nbr) {
+        int[] values = {50, 40, 10, 9, 5, 4, 1};
+        String[] romans = {"L", "XL", "X", "IX", "V", "IV", "I"};
 
-    public String convert(int number) {
-        if (number == 1) {
-            return "I";
-        }
-        if (number == 3) {
-            return "III";
-        }
-        if (number == 4) {
-            return "IV";
-        }
-        if (number == 10) {
-            return "X";
-        }
-        if (number == 39) {
-            return "XXXIX";
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (nbr >= values[i]) {
+                result.append(romans[i]);
+                nbr -= values[i];
+            }
         }
 
-        return "";
+        return result.toString();
     }
 }
+
