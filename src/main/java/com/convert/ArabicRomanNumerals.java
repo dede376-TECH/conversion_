92be61e596833f7a655af90cdd49d70fc766e3c8
dede376-TECH <khamlicheabdelghani5@gmail.com>
2024-12-18 22:02:46 +0100

    Ajout du code de conversion pour 1

--- src/main/java/com/convert/ArabicRomanNumerals.java
+++ src/main/java/com/convert/ArabicRomanNumerals.java
@@ -2,9 +2,9 @@ package com.convert;
 
 public class ArabicRomanNumerals {
 
-    public String convert(int nbr) {
-        if (nbr == 1) {
-            return "I";  // Code minimal pour faire passer le test
+    public String convert(int number) {
+        if (number == 1) {
+            return "I";
         }
         return "";
     }
